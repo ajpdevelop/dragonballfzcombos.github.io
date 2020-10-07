77b31c865e82c76907da0ae4cea404024348e5ac
Anthony <ajpdevelop@gmail.com>
2020-10-07 15:30:37 -0400

    Add bg image for header + new font

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,4 +8,8 @@
   .row {
     text-align: center;
   }
+  h2 {
+    font-family: "LeviReBrushed", Helvetica, Arial;
+    text-shadow: 0 0 1px rgb(247, 158, 22), 0 0 1px rgb(247, 158, 22), 0 0 1px rgb(247, 158, 22), 0 0 1px rgb(247, 158, 22);
+  }
 </style>
