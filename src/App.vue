4b097d6683a730498922101214a093ccb82539e4
Anthony <ajpdevelop@gmail.com>
2020-10-11 08:24:24 -0400

    setting up tabs

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,14 @@
         <character-list />
     </v-navigation-drawer>
 
-    <v-app-bar dense app>
+    <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="navHeader">Dragonball FighterZ Combos!</v-toolbar-title>
       
       <div class="flex-grow-1"></div>
 
       <v-toolbar-items>
+        <v-btn v-text="activeTab"></v-btn>
         <v-btn>About</v-btn>
         <v-btn>Suggestions</v-btn>
         <v-btn>Contact</v-btn>
@@ -19,6 +20,19 @@
 
     <v-main>
       <v-container>
+        <v-row>
+          <v-tabs centered v-model="tab" background-color="primary" dark>
+              <v-tab large @click="characterTabActive('characterTab1')">
+                  Character One
+              </v-tab>
+              <v-tab large @click="characterTabActive('characterTab2')">
+                  Character Two
+              </v-tab>
+              <v-tab large @click="characterTabActive('characterTab3')">
+                  Character Three
+              </v-tab>
+          </v-tabs>
+        </v-row>
         <v-row >
           <v-col cols="12">
             <character-details />
@@ -32,27 +46,34 @@
 <script>
 import CharacterDetails from "./components/CharacterDetails";
 import CharacterList from "./components/CharacterList";
-import { VApp, VMain, VContainer, VRow, VCol, VNavigationDrawer, VAppBar, VAppBarNavIcon, VToolbarTitle, VToolbarItems, VBtn } from "vuetify/lib/components";
+import { VApp, VMain, VContainer, VRow, VCol, VNavigationDrawer, VAppBar, VAppBarNavIcon, VToolbarTitle, VToolbarItems, VBtn, VTabs, VTab } from "vuetify/lib/components";
 
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({ 
+    drawer: null,
+    activeTab: null
+  }),
   components: {
     CharacterList,
     CharacterDetails,
-    VApp, VMain, VContainer, VRow, VCol, VNavigationDrawer, VAppBar, VAppBarNavIcon, VToolbarTitle, VToolbarItems, VBtn
+    VApp, VMain, VContainer, VRow, VCol, VNavigationDrawer, VAppBar, VAppBarNavIcon, VToolbarTitle, VToolbarItems, VBtn, VTabs, VTab
+  },
+  computed: {
+  },
+  methods: {
+    characterTabActive(characterTab) {
+        return this.$store.commit("setActiveCharacterTab", characterTab);
+    }
   }
 };
 </script>
 
 <style>
-  @font-face {
+@font-face {
     font-family: "LeviReBrushed";
     src: local("LeviReBrushed"),
     url(./fonts/LeviReBrushed.ttf) format("truetype");
   }
-</style>
-
-<style>
 .nav-drawer {
   background-color: var(--v-primary-base)!important;
 }
