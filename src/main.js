08822ee3369512f3fa492be5043b671d15360a35
Anthony <ajpdevelop@gmail.com>
2020-11-04 12:28:41 -0500

    Save firebase & add firebase analytics

--- src/main.js
+++ src/main.js
@@ -4,6 +4,22 @@ import App from './App.vue';
 import Vuetify from 'vuetify/lib';
 import colors from 'vuetify/lib/util/colors';
 
+import firebase from 'firebase';
+    
+const firebaseConfig = {
+    apiKey: "AIzaSyDlYmErW5iSnyctegf8unpF_4hW6AwKqZw",
+    authDomain: "dragonball-fighterz-combos.firebaseapp.com",
+    databaseURL: "https://dragonball-fighterz-combos.firebaseio.com",
+    projectId: "dragonball-fighterz-combos",
+    storageBucket: "dragonball-fighterz-combos.appspot.com",
+    messagingSenderId: "774171031330",
+    appId: "1:774171031330:web:e0f21c0d12711f01af177b",
+    measurementId: "G-GGJ2BE99EP"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 Vue.config.productionTip = false
 
 Vue.use(Vuetify);
